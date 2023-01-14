7eb8d7874d69dde1a7f0348f0e5d30cdedcded76
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-01-14 17:59:43 +0900

    CLI tests (cli-test-1673686783002)

--- app/server.go
+++ app/server.go
@@ -18,12 +18,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	for {
+	receiveTCPConnection(l)
+}
+
+func receiveTCPConnection(l net.Listener){
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go conn.Write([]byte("+PONG\r\n"))
+		redisHandler(conn)
+}
+
+func redisHandler(conn net.Conn) {
+	defer conn.Close()
+	for {
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
