91b14d1aa6557952b881772494e5dfa7ad3457ff
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-02-18 21:10:35 +0900

    CLI tests (cli-test-1676722235609)

--- app/server.go
+++ app/server.go
@@ -12,38 +12,46 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
+	// リッスンの開始
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
+		// プログラムを終了させる（0は成功、0以外はエラーを示す）
 		os.Exit(1)
 	}
 
-	receiveTCPConnection(l)
+	go receiveTCPConnection(l)
 }
 
 func receiveTCPConnection(l net.Listener){
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		defer conn.Close()
+	// net.listenで得たリスナーへの接続を待機し、返す。
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	// 接続を閉じる。receiveTCPConnection関数の終了時に実行される。
+	defer conn.Close()
 
-		for {
-			buf := make([]byte, 1024)
-			_, err := conn.Read(buf)
-			if err == io.EOF {
-				fmt.Println("Connection closed")
-				break
-			}
-			if err != nil {
-				fmt.Println("Error reading:", err.Error())
-			}
-			redisHandler(conn)
+	for {
+		buf := make([]byte, 1024)
+		// コネクションからデータを読み取る
+		fmt.Println(1)
+		_, err := conn.Read(buf)
+		fmt.Println(2)
+		if err == io.EOF {
+			fmt.Println("Connection closed")
+			break
 		}
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		redisHandler(conn)
+	}
 }
 
 func redisHandler(conn net.Conn) {
+	fmt.Println("call redisHandler")
+	// コネクションにデータを書き込む
 	conn.Write([]byte("+PONG\r\n"))
 }
