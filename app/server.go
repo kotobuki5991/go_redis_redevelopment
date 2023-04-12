5d16bb662e7214c37d8b9f142f775adb0144db53
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-04-13 00:26:57 +0900

    CLI tests (cli-test-1681313217887)

--- app/server.go
+++ app/server.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// リッスンの開始
@@ -32,29 +32,51 @@ func createRedisRequestReceiver(l net.Listener){
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	// 接続を閉じる。receiveTCPConnection関数の終了時に実行される。
+	// 接続を閉じる。createRedisRequestReceiver関数の終了時に実行される。
 	defer conn.Close()
 	redisHandler(conn)
 }
 
 func redisHandler(conn net.Conn){
-	fmt.Println("call redisHandler")
-
-	for {
-		buf := make([]byte, 1024)
-		// コネクションからデータを読み取る
-		fmt.Println(1)
-		_, err := conn.Read(buf)
-		fmt.Println(2)
-		if err == io.EOF {
-			fmt.Println("Connection closed")
-			break
-		}
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-		}
-		writeResponse(conn)
+	input, err := bufio.NewReader(conn).ReadBytes(byte('\n'))
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return
 	}
+	fmt.Println(input)
+}
+
+// func redisHandler(conn net.Conn){
+// 	fmt.Println("call redisHandler")
+
+// 	for {
+// 		buf := make([]byte, 1024)
+// 		// コネクションからデータを読み取る
+// 		_, err := conn.Read(buf)
+// 		if err == io.EOF {
+// 			fmt.Println("Connection closed")
+// 			break
+// 		}
+// 		if err != nil {
+// 			fmt.Println("Error reading:", err.Error())
+// 		}
+
+// 		_, err = conn.Write(buf)
+// 		if err != nil {
+// 			fmt.Println("Error writing:", err.Error())
+// 		}
+
+// 		writeResponse(conn)
+// 	}
+// }
+
+// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\nを受け取った場合
+func isEchoCmd(b []byte){
+	artIndex := bytes.Index(b, []byte("\r\n"))
+	// "\r\n"が存在しない場合処理終了
+	if artIndex == -1 {return}
+	// 区切り文字で分轄
+	// slice := bytes.Split(b, []byte("\r\n"))
 }
 
 func writeResponse(conn net.Conn) {
