a009f1281daa9e562ab0e0a49240f75df95d1f0f
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-04-13 00:15:12 +0900

    CLI tests (cli-test-1681312512148)

--- app/server.go
+++ app/server.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// リッスンの開始
@@ -32,7 +31,7 @@ func createRedisRequestReceiver(l net.Listener){
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	// 接続を閉じる。receiveTCPConnection関数の終了時に実行される。
+	// 接続を閉じる。createRedisRequestReceiver関数の終了時に実行される。
 	defer conn.Close()
 	redisHandler(conn)
 }
@@ -53,8 +52,24 @@ func redisHandler(conn net.Conn){
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
 		}
+
+		_, err = conn.Write(buf)
+		if err != nil {
+			fmt.Println("Error writing:", err.Error())
+		}
+
 		writeResponse(conn)
 	}
+}
+
+// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\nを受け取った場合
+func isEchoCmd(b []byte){
+	artIndex := bytes.Index(b, []byte("\r\n"))
+	// "\r\n"が存在しない場合処理終了
+	if artIndex == -1 {return}
+	// 区切り文字で分轄
+	slice := bytes.Split(b, []byte("\r\n"))
+	
 }
 
 func writeResponse(conn net.Conn) {
